c52b890d53e20a3cf999ffc0911c042ef1ed688c
Nic Fusciardi <nic@pragmatic-web.co.uk>
2018-09-06 14:13:37 +0100

    added input test

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,12 @@
 def hello_world():
     return 123
+
+
+def input_number(message):
+    while True:
+        try:
+            user_input = int(message)
+        except:
+            raise Exception("Not an integer! Try again.")
+        else:
+            return user_input
--- hello_world_test.py
+++ hello_world_test.py
@@ -5,3 +5,6 @@ from hello_world import *
 class MyFirstTests(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello_world(), 'hello world')
+
+    def test_input_number(self):
+        self.assertEqual(input_number('two'), 11)
